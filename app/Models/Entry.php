c5e211056fca5de594d47c7403bc82b7b5e6f741
Fatih Kahveci <f.kahveci@ymail.com>
2015-06-22 12:25:42 +0300

    directory change

---
+++ app/Models/Entry.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+
+    protected $table = 'entries';
+
+    protected $fillable = ['content', 'status'];
+
+}
